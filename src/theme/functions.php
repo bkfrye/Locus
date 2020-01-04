bdbdf654af96c3f13aa2ffefd8cf5d7a87ac62c7
Brandon Frye <bfrye@centerline.net>
2020-01-03 23:08:03 -0500

    added news links CPT

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -172,3 +172,68 @@ function getMetaData($url) {
     endif;
   echo '</div>';
 }
+
+
+// Register Custom Post Type
+function news_links() {
+
+	$labels = array(
+		'name'                  => _x( 'News Links', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'News Link', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'News Links', 'text_domain' ),
+		'name_admin_bar'        => __( 'News Link', 'text_domain' ),
+		'archives'              => __( 'Link Archives', 'text_domain' ),
+		'attributes'            => __( 'Link Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( '', 'text_domain' ),
+		'all_items'             => __( 'All Links', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Link', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Link', 'text_domain' ),
+		'edit_item'             => __( 'Edit Link', 'text_domain' ),
+		'update_item'           => __( 'Update Link', 'text_domain' ),
+		'view_item'             => __( 'View Link', 'text_domain' ),
+		'view_items'            => __( 'View Links', 'text_domain' ),
+		'search_items'          => __( 'Search Link', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                  => 'news-links',
+		'with_front'            => true,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => __( 'News Link', 'text_domain' ),
+		'description'           => __( 'News link posts', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'custom-fields' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 20,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => false,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'post',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'news_links', $args );
+
+}
+add_action( 'init', 'news_links', 0 );
