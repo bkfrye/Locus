17da6085ce17587dd81081395f9c91fd440d367f
Brandon Frye <brandonkfrye@gmail.com>
2020-09-08 09:00:22 -0400

    comment out WP versioning

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -22,18 +22,18 @@ add_action( 'wp_enqueue_scripts', 'locus_resources' );
  * appended to scripts and styles
  */
 // remove version from head
-remove_action('wp_head', 'wp_generator');
-// remove version from rss
-add_filter('the_generator', '__return_empty_string');
-// remove version from scripts and styles
-function remove_version_scripts_styles($src) {
-	if (strpos($src, 'ver=')) {
-		$src = remove_query_arg('ver', $src);
-	}
-	return $src;
-}
-add_filter('style_loader_src', 'remove_version_scripts_styles', 9999);
-add_filter('script_loader_src', 'remove_version_scripts_styles', 9999);
+// remove_action('wp_head', 'wp_generator');
+// // remove version from rss
+// add_filter('the_generator', '__return_empty_string');
+// // remove version from scripts and styles
+// function remove_version_scripts_styles($src) {
+// 	if (strpos($src, 'ver=')) {
+// 		$src = remove_query_arg('ver', $src);
+// 	}
+// 	return $src;
+// }
+// add_filter('style_loader_src', 'remove_version_scripts_styles', 9999);
+// add_filter('script_loader_src', 'remove_version_scripts_styles', 9999);
 
 
 /**
