157ee68384925383f2cd74a19ac5c7737115f119
Brandon Frye <brandonkfrye@gmail.com>
2020-09-11 14:31:59 -0400

    :bug: fixed GA id issue

--- src/theme/header.php
+++ src/theme/header.php
@@ -7,7 +7,7 @@
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', <?php the_field('google_analytics_id', 'option'); ?>);
+    gtag('config', '<?php the_field('google_analytics_id', 'option'); ?>');
   </script>
 
 
