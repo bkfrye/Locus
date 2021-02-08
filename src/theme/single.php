a3f43b05e84c625e0355dfe82b660c5cc72a6e76
Brandon Frye <brandonkfrye@gmail.com>
2021-02-07 20:21:26 -0500

    add templates for cpts

--- src/theme/single.php
+++ src/theme/single.php
@@ -1,29 +1,21 @@
 <?php get_header(); ?>
 <section class="wrapper">
-	<div class="site-content" style="padding: 15px;margin-top: 100px;">
-		<div class="employee-bio-panel">
-			<div class="employee-bio-wrapper">
-				<header class="employee-bio">
-					<div class="employee-image">
-						<?php
-							$image = get_field('profile_image');
-							$size = 'medium'; // (thumbnail, medium, large, full or custom size)
-							if( $image ) {
-								echo wp_get_attachment_image( $image, $size );
-							}
-						?>
-					</div>
-					<div>
-						<p class="employee-role"><?php echo str_replace('_', ' ', $type);?></p>
-						<?php the_title('<h3 class="employee-name">','</h3>'); ?>
-						<p class="employee-title">
-							<?php the_field('role');?>
-						</p>
-					</div>
-				</header>
-				<div class="employee-content"><?php the_field('biography');?></div>
-			</div>
-		</div>
+	<div class="site-content single" style="padding: 15px;margin-top: 100px;">
+		<?php
+		if ( have_posts() ) :
+			while ( have_posts() ) :
+				the_post();
+				if ( get_post_format() == false ) {
+					get_template_part( 'content', 'single' );
+				} else {
+					get_template_part( 'content', 'employees' );
+				}
+		endwhile;
+		else :
+			get_template_part( 'content', 'none' );
+		endif;
+		?>
 	</div>
 </section>
+
 <?php get_footer(); ?>
