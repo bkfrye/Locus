4ff654d41e047a0b32b03e06f0d958b336777180
bkfrye <brandonkfrye@gmail.com>
2021-08-06 15:30:20 -0400

    ensure that asset link versions are not added for cache busting

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -12,6 +12,15 @@ function locus_resources()
 
 add_action('wp_enqueue_scripts', 'locus_resources');
 
+// remove wp version number from scripts and styles
+function remove_css_js_version( $src ) {
+  if( strpos( $src, '?ver=' ) )
+      $src = remove_query_arg( 'ver', $src );
+  return $src;
+}
+add_filter( 'style_loader_src', 'remove_css_js_version', 9999 );
+add_filter( 'script_loader_src', 'remove_css_js_version', 9999 );
+
 // function custom_excerpt_length()
 // {
 // 	return 22;
