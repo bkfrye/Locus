2b427d45b94b5cbe5de8d53bbf90aa3d2fbe3864
bkfrye <brandonkfrye@gmail.com>
2021-08-10 16:10:09 -0400

    add slick carousel to prod build

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,10 @@ const pluginsListProd = [
 /* -------------------------------------------------------------------------------------------------
 Header & Footer JavaScript Boundles
 -------------------------------------------------------------------------------------------------- */
-const headerJS = [ './node_modules/jquery/dist/jquery.js' ];
+const headerJS = [ 
+	'./node_modules/jquery/dist/jquery.js',
+	'./node_modules/slick-carousel/slick/slick.min.js' 
+];
 
 const footerJS = [ './src/assets/js/**' ];
 
