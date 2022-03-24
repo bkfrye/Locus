c7a7ef0a8b1f672baad19ea0349fffd05d7c00a3
bkfrye <brandonkfrye@gmail.com>
2022-03-24 17:23:17 -0400

    include external linking feature to most recent post

--- src/theme/content-featured.php
+++ src/theme/content-featured.php
@@ -2,6 +2,13 @@
 <div class="featured-news-content">
   <h2 class="featured-headline"><?php the_title(); ?></h2>
   <div class="btn white">
-    <a href="<?php echo get_the_permalink(); ?>" target="_blank">Read More</a>
+    <?php
+      if ( get_field('news_url') ) :
+        $news_url = get_field('news_url');
+      else :
+        $news_url = get_the_permalink();
+      endif;
+    ?>
+    <a href="<?php echo $news_url; ?>" target="_blank">Read More</a>
   </div>
 </div>
